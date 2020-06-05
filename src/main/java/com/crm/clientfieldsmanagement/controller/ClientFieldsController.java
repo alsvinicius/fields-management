4b028f522fb4e8ec0e09fe7f59d28729b9c01191
Vinicius <als.vinicius@gmail.com>
2020-06-04 21:41:43 -0300

    improvement: Swagger UI for /api/** endpoints

--- src/main/java/com/crm/clientfieldsmanagement/controller/ClientFieldsController.java
+++ src/main/java/com/crm/clientfieldsmanagement/controller/ClientFieldsController.java
@@ -2,8 +2,11 @@ package com.crm.clientfieldsmanagement.controller;
 
 import com.crm.clientfieldsmanagement.model.ClientField;
 import com.crm.clientfieldsmanagement.service.ClientFieldsService;
+import com.sun.org.apache.xpath.internal.objects.XString;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,34 +16,41 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/management/client/field")
+@Api(tags = "Client Fields", produces = "application/json")
 public class ClientFieldsController {
 
     @Autowired
     private ClientFieldsService service;
 
-    @GetMapping
+    private static final String RESPONSE_TYPE = "application/json";
+
+    @GetMapping(produces = RESPONSE_TYPE)
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody List<ClientField> findAll() {
+    public @ResponseBody
+    List<ClientField> findAll() {
         return service.findAll();
     }
 
-    @PostMapping
+    @PostMapping(produces = RESPONSE_TYPE)
     @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody ClientField create(@RequestBody ClientField field) {
+    public @ResponseBody
+    ClientField create(@RequestBody ClientField field) {
         return service.create(field);
     }
 
-    @GetMapping("/{fieldId}")
+    @GetMapping(value = "/{fieldId}", produces = RESPONSE_TYPE)
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody ClientField findOne(@PathVariable String fieldId) {
+    public @ResponseBody
+    ClientField findOne(@PathVariable String fieldId) {
         return service.findOne(fieldId);
     }
 
-    @PatchMapping("/{fieldId}")
+    @PatchMapping(value = "/{fieldId}", produces = RESPONSE_TYPE)
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody ClientField update(
-        @PathVariable String fieldId,
-        @RequestBody ClientField field
+    public @ResponseBody
+    ClientField update(
+            @PathVariable String fieldId,
+            @RequestBody ClientField field
     ) {
         return service.update(fieldId, field);
     }
@@ -50,7 +60,6 @@ public class ClientFieldsController {
     public void delete(@PathVariable String fieldId) {
         service.delete(fieldId);
     }
-
 
 
 }
